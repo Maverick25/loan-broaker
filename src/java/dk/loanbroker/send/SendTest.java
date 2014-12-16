ed36935050d18b4a5b8ec4342030e78113f14e4f
Maverick25 <rigan.marek@gmail.com>
2014-12-16 14:05:14 +0100

    test

--- src/java/dk/loanbroker/send/SendTest.java
+++ src/java/dk/loanbroker/send/SendTest.java
@@ -5,17 +5,44 @@
  */
 package dk.loanbroker.send;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+import java.io.IOException;
+
 /**
  *
  * @author marekrigan
  */
-public class SendTest {
+public class SendTest 
+{
 
+    private static final String TASK_QUEUE_NAME = "task_queue";
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException
+    {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("datdb.cphbusiness.dk");
+	factory.setUsername("student");
+	factory.setPassword("cph");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+        String message = "What's up Get Credit Score?!";
+
+        channel.basicPublish( "", TASK_QUEUE_NAME, 
+                MessageProperties.PERSISTENT_TEXT_PLAIN,
+                message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+
+        channel.close();
+        connection.close();
     }
     
 }
