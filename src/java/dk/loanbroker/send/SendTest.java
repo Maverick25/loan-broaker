c0b3335e0971928123829e7171567e65b2b2b6f4
Maverick25 <rigan.marek@gmail.com>
2014-12-16 13:19:06 +0100

    test

---
+++ src/java/dk/loanbroker/send/SendTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.loanbroker.send;
+
+/**
+ *
+ * @author marekrigan
+ */
+public class SendTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
